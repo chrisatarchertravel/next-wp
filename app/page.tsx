743e8f898e5834b593abe97eb6c15470f3734886
Chris La Barba <chris@archertravel.com>
2025-09-24 10:43:59 -0700

    remove uneeded pages

--- app/page.tsx
+++ app/page.tsx
@@ -73,18 +73,6 @@ const ToDelete = () => {
       <div className="grid md:grid-cols-3 gap-4 mt-6">
         <Link
           className="border h-48 bg-accent/50 rounded-lg p-4 flex flex-col justify-between hover:scale-[1.02] transition-all"
-          href="/posts"
-        >
-          <Pen size={32} />
-          <span>
-            Posts{" "}
-            <span className="block text-sm text-muted-foreground">
-              All posts from your WordPress
-            </span>
-          </span>
-        </Link>
-        <Link
-          className="border h-48 bg-accent/50 rounded-lg p-4 flex flex-col justify-between hover:scale-[1.02] transition-all"
           href="/pages"
         >
           <File size={32} />
@@ -92,18 +80,6 @@ const ToDelete = () => {
             Pages{" "}
             <span className="block text-sm text-muted-foreground">
               Custom pages from your WordPress
-            </span>
-          </span>
-        </Link>
-        <Link
-          className="border h-48 bg-accent/50 rounded-lg p-4 flex flex-col justify-between hover:scale-[1.02] transition-all"
-          href="/posts/authors"
-        >
-          <User size={32} />
-          <span>
-            Authors{" "}
-            <span className="block text-sm text-muted-foreground">
-              List of the authors from your WordPress
             </span>
           </span>
         </Link>
